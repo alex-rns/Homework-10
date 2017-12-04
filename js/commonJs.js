2ed29f8441a6f32e80e81dc81f40c605bb3e54a5
alex-rns <alexrns3@gmail.com>
2017-12-05 00:04:06 +0200

    gallery

--- js/commonJs.js
+++ js/commonJs.js
@@ -1,12 +1,4 @@
 $(document).ready(function () {
 
-    $('.works-item').on('mouseenter', function () {
-        $('.works-item-info h3').css('color', 'gray')
-    });
-    $('.works-item').on('mouseleave', function () {
-        $('.works-item-info h3').css('color', 'red')
-    });
-
-
 
 });
