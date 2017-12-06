2e56ca6ff08030199d107bfc6c59c1fbe090c65a
alex-rns <alexrns3@gmail.com>
2017-12-06 22:24:05 +0200

    menu jq

--- js/commonJs.js
+++ js/commonJs.js
@@ -1,3 +1,34 @@
 $(document).ready(function () {
 
+    $(window).on('scroll', function () {
+       if($(window).scrollTop() >=50){
+        $('.main-header').addClass('fixed-header')
+       }
+       else {
+        $('.main-header').removeClass('fixed-header')
+       }
+
+        if($(window).scrollTop() >=2450){
+            $('.button-animated').addClass('animated tada')
+        }
+    });
+
+    $('.navigation-button').on('click', function () {
+       $(this).toggleClass('navigation-button-open');
+       $('.navigation').toggleClass('navigation-mobile').css('display', 'block')
+    });
+
+
+
+    $('.slick-carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        arrows: false
+    });
+
+
 });
+
