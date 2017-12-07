d74a01cce7d5b6017882aadca1e0bfa5bc587776
alex-rns <alexrns3@gmail.com>
2017-12-07 17:34:22 +0200

    anhor

--- js/commonJs.js
+++ js/commonJs.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
 
+    // Fixed header
     $(window).on('scroll', function () {
        if($(window).scrollTop() >=50){
-        $('.main-header').addClass('fixed-header')
+        $('.main-header').addClass('fixed-header');
+        $('.header-wrap').removeClass('header-wrap-border');
        }
        else {
-        $('.main-header').removeClass('fixed-header')
+        $('.main-header').removeClass('fixed-header');
+        $('.header-wrap').addClass('header-wrap-border');
        }
 
         if($(window).scrollTop() >=2450){
@@ -13,13 +16,15 @@ $(document).ready(function () {
         }
     });
 
+
+
     $('.navigation-button').on('click', function () {
        $(this).toggleClass('navigation-button-open');
        $('.navigation').toggleClass('navigation-mobile').css('display', 'block')
     });
 
 
-
+    // Slider
     $('.slick-carousel').slick({
         dots: true,
         infinite: true,
@@ -28,6 +33,15 @@ $(document).ready(function () {
         cssEase: 'linear',
         arrows: false
     });
+
+
+    // Плавная прокрутка по якорям
+    $('a[href^="#"]').on('click', function () {
+        let anhor = $(this).attr('href');
+        $('html, body').animate({scrollTop: $(anhor).offset().top - 65}, 800);
+        return false
+    });
+
 
 
 });
